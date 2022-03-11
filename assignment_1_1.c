59f151d69a5797185632e8f22de34d05dc6dfbc8
SAVSANI SEJAL <71888563+sejalsavasani-1914@users.noreply.github.com>
2022-03-11 23:34:12 +0530

    Update assignment_1_1.c

--- assignment_1_1.c
+++ assignment_1_1.c
@@ -7,7 +7,7 @@ int main()
 	int fd,fd1,len;
 	char r[50];
 	fd = open("assignment1.txt",O_CREAT | O_RDONLY);
-	fd = open("write1.txt",O_WRONLY);
+	fd1 = open("write1.txt",O_WRONLY);
 	len = read(fd, r , 20);
 	if(write(fd1,r,strlen(r))==-1)
 	{

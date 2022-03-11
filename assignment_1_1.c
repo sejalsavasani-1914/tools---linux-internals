9b7936ededd7a2def92b0ae007c08df1e89d7d60
SAVSANI SEJAL <71888563+sejalsavasani-1914@users.noreply.github.com>
2022-03-11 23:30:02 +0530

    Add files via upload

---
+++ assignment_1_1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+int main()
+{
+	int fd,fd1,len;
+	char r[50];
+	fd = open("assignment1.txt",O_CREAT | O_RDONLY);
+	fd = open("write1.txt",O_WRONLY);
+	len = read(fd, r , 20);
+	if(write(fd1,r,strlen(r))==-1)
+	{
+		printf("error\n");
+		exit(0);
+	}
+	else
+	{
+		printf("written bytes = %d\n",strlen(r));
+	}
+	close(fd);
+	return 0;
+}

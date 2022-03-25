9371d8e0ac49757ec9db834232de513647a2dbab
SAVSANI SEJAL <71888563+sejalsavasani-1914@users.noreply.github.com>
2022-03-25 10:25:08 +0530

    Add files via upload

---
+++ assignment-3/ass_3_1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<stdlib.h>
+
+//create global variable
+int a=0;
+
+pthread_t tid;
+
+void *thread(void *arg)
+{
+    int *id = (int *)arg;
+	printf("inside thread.\n");
+    ++a;
+    printf("Thread id is = %d, global variable value = %d\n",*id,a);
+	return NULL;
+}
+
+int main(void)
+{
+    for(int i=0;i<3;i++)
+    {
+    	pthread_create(&tid,NULL,thread,(void *)&tid);
+    }
+    pthread_exit(NULL);
+return 0;
+}
